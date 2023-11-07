c754fd66e5c1184b423630b54b3c3073986fef31
rahulvk007 <rahulvijayakumarpt4551@gmail.com>
2023-11-07 14:10:43 +0530

    added templates and static to flask

--- main/__init__.py
+++ main/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, redirect, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import azure.functions as func
 
 app = Flask(__name__)
@@ -17,3 +17,21 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
 @app.route('/')
 def index():
     return "Hello World, Simple Flask API Hosted in Azure Functions"
+
+@app.route("/form",methods=['POST','GET'])
+def home():
+    if request.method=='POST':
+        details = request.form
+        dict = {}
+        dict['reg_no']=details["regno"]
+        dict['name'] = details["name"]
+        dict['club_dept']='FFCS'
+        dict['phone_no'] = details["phoneno"]
+        dict['email'] = details["email"]
+        dict['score'] = 0
+        dict['contributions'] = 0
+        dict['expertize_in'] = details["exp"]
+        dict['contribution_details'] = ''
+        return "Success"
+    
+    return render_template("form.html")
